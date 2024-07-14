f9a8f621159c9c0ff447f633a4572331db798ab8
Izaque Sena dos Santos <isantos@brex.com>
2024-07-14 19:23:49 -0300

    3 e 4

--- AtividadesProcessos/3.cpp
+++ AtividadesProcessos/3.cpp
@@ -11,7 +11,7 @@ gcc -pthread ...
 
 int iteracoes = 1000;
 
-void worker(){
+void* worker(void *thread_id){
 	for (int i=0; i<iteracoes; i++){
 		for (int j=0; j<iteracoes; j++){
 			for (int k=0; k<iteracoes; k++){
@@ -23,5 +23,12 @@ void worker(){
 
 int main(int argc, char * argv[]){
 	int n_threads = atol(argv[1]);
+	pthread_t threads[n_threads];
+	for(int t = 0; t < n_threads; t++ )
+		pthread_create(&threads[t], NULL, worker, (void *)t);
+
+	for( int i = 0; i < n_threads; i++ )
+		pthread_join(threads[i], NULL);
+
 	return 0;
 }
--- AtividadesProcessos/4.cpp
+++ AtividadesProcessos/4.cpp
@@ -2,6 +2,7 @@
 gcc -pthread ... 
 */
 
+#include <iostream>
 #include <stdio.h>
 #include <sys/mman.h>
 #include <sys/types.h>
@@ -10,10 +11,44 @@ gcc -pthread ...
 #include <pthread.h>
 
 int number = -1;
+int created = 0;
 std::vector<int> vet;
+
+void* workerA(void *thread_id) {
+    std::cout << "Thread A: Digite o tamanho do vetor: ";
+    std::cin >> number;
+}
+
+void* workerB(void *thread_id) {
+    while(number == -1) {
+
+    }
+    if (number > 0) {
+        for(int i = 0; i < number; ++i) {
+            vet.push_back(i);
+        }
+        created = 1;
+    }
+}
+
+void* workerC(void *thread_id) {
+    while (created == 0) {
+
+    }
+    std::cout << "Thread C: Elementos do vetor: ";
+    for(const int& val : vet) {
+        std::cout << val << " ";
+    }
+}
 
 int main(int argc, char * argv[])
 {	
-	while(1);
+	pthread_t threads[3];
+	pthread_create(&threads[0], NULL, workerC, (void *)0);
+    pthread_create(&threads[1], NULL, workerB, (void *)1);
+    pthread_create(&threads[2], NULL, workerA, (void *)2);
+
+    for( int i = 0; i < 3; i++ )
+		pthread_join(threads[i], NULL);
     return 0;
 }

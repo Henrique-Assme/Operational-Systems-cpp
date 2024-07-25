edbb43729edb25743161a4772d87c09f70db4cd3
Henrique-Assme <heniassme@gmail.com>
2024-07-25 16:56:54 -0300

    3 done

--- AtividadeSincronizacaoDeadlocks/3.cpp
+++ AtividadeSincronizacaoDeadlocks/3.cpp
@@ -6,6 +6,9 @@
 #include <unistd.h>
 #include <limits>
 
+#define BUFFER_SIZE 15
+pthread_mutex_t mutex;
+
 /*NÃO ALTERE ESSA VARIAVEL*/
 int N = 0;
 
@@ -15,17 +18,28 @@ void print_buffer(int i){
 }
 
 void* produtor(void *thread_id){
-	while(1)
-	{
-	print_buffer(++N);//Funciona se trocarmos por N++?
-	printf("\n");
+	while(1){
+		pthread_mutex_lock(&mutex);
+		if(N < BUFFER_SIZE) {
+			print_buffer(N++); 
+			printf("\n");
+		} else {
+			// printf("Buffer lotado");
+		}
+		pthread_mutex_unlock(&mutex); 
 	}
 }
 
 void* consumidor(void *thread_id){
-	while(1){
-	print_buffer(--N);//Funciona se trocarmos por N--?
-	printf("\n");	
+	while (1) {
+		pthread_mutex_lock(&mutex);
+		if(N > 0) {
+			print_buffer(N--); 
+			printf("\n");
+		} else {
+			// printf("Buffer vazio");
+		}
+		pthread_mutex_unlock(&mutex);
 	}
 }
 
@@ -36,7 +50,7 @@ int main(int argc, char * argv[]){
 	for(int t = 0; t < produtores; t++)
 		pthread_create(&threads[t], NULL, produtor, (void *)t);
 
-	for (int t =0; t<consumidores;t++)
+	for (int t = consumidores; t<consumidores+produtores;t++)
 		pthread_create(&threads[t], NULL, consumidor, (void *)t);
 	
 	while(1);
